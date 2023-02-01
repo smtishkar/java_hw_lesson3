7bb604be750d76c67568a7af7d0280c0ad6a6882
Sergey <smtishkar@mail.ru>
2023-02-01 09:30:21 +0300

    корректный подсчет с установленными блоками

--- test.java
+++ test.java
@@ -3,29 +3,29 @@
  */
 public class test {
 
-
-
     public static void main(String[] args) {
 
-        int n = 9;
-        int m = 9;
+        int n = 9;                      // длина поля
+        int m = 9;                      // ширина поля
         int[][] arr = new int[n][m];
-        int [] blocks = new int [] {19, 27};        // Указать тут номера ячеек где поставим блоки
- 
+        int[] blocks = new int[] {19, 35, 28, 30}; // Указать тут номера ячеек где поставим блоки
 
-        arrOneByOneFilling(arr,n,m);
-        print(arr, n, m);
+        arrOneByOneFilling(arr, n, m);
+        print(arr, n, m);               //печать поля запорлненного цифрами по порядку
         System.out.println();
-        
-        int [][] array = blocksprep(arr,n,m,blocks);
-        print(array, n, m);
+
+        int[][] array = blocksprep(arr, n, m, blocks);
+        print(array, n, m);             // печать поля с установленными блоками
         System.out.println();
 
         calc(array, n, m);
-        print(array, n, m);
+        print(array, n, m);             // печать поля с отоброжением количества ходов в каждой ячейке
+        System.out.println();
+        System.out.println();
+        System.out.println("Максимальное количество ходов равно = " + array[n - 1][m - 1]);
+        System.out.println();
 
     }
-
 
     public static int[][] arrOneByOneFilling(int arr[][], int n, int m) {
         int num = 0;
@@ -38,18 +38,17 @@ public class test {
         return arr;
     }
 
-    public static int [][] blocksprep (int arr[][], int n, int m, int arr2 [])
-        {
+    public static int[][] blocksprep(int arr[][], int n, int m, int arr2[]) {
         int count = 0;
         while (count < arr2.length) {
             for (int i = 1; i < n; i++) {
                 for (int j = 1; j < m; j++) {
-                    if (arr[i][j] == arr2[count]){
+                    if (arr[i][j] == arr2[count]) {
                         arr[i][j] = 0;
-                    } 
+                    }
                 }
             }
-        count +=1;
+            count += 1;
         }
         return arr;
     }
@@ -67,32 +66,29 @@ public class test {
     public static int[][] calc(int arr[][], int n, int m) {
         for (int i = 1; i < n; i++) {
             for (int j = 1; j < m; j++) {
-                if (arr[i][j] == 0){
-                    // arr[i][j+1] = arr [i-1][j]+1;
-                    arr[i][j+1] = -1;
-                    System.out.println("12");
-                }
-                else{
-                    if (i == 1) {
-                        arr[i][j] = 1;
-                    } else {
-                        int res = arr[i][j - 1] + arr[i - 1][j];
-                        arr[i][j] = res;
-                        // System.out.println(arr[i][j] + "\t");
-                }
-                // if (i == 1) {
-                //     arr[i][j] = 1;
-                // } else {
-                //     int res = arr[i][j - 1] + arr[i - 1][j];
-                //     arr[i][j] = res;
-                //     System.out.println(arr[i][j] + "\t");
 
+                if (i == 1 || j == 1) {
+                    arr[i][j] = 1;
+                } else if (arr[i][j] == 0) {
+                    arr[i][j] = 0;
                 }
-
+                else if (arr[i][j - 1] + arr[i - 1][j] == 0) {
+                    arr[i][j] = 0;
+                }
+                else if (arr[i][j - 1] == 0) {
+                    int res = arr[i - 1][j] + 1;
+                    arr[i][j] = res;
+                } 
+                else if (arr[i-1][j] == 0){
+                    int res = arr[i][j-1] + 1;
+                    arr[i][j] = res;
+                }
+                else {
+                    int res = arr[i][j - 1] + arr[i - 1][j];
+                    arr[i][j] = res;
+                }
             }
-        
         }
         return arr;
     }
-
 }
